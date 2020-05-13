ea35d2815b10ffd0916814c4c9efce11a2996e83
samuelskinstad <s_skinstad@hotmail.com>
2020-05-13 19:16:11 +0200

    clickincrease

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,20 +9,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1e2c35),
       body: Center(
-        child: RaisedButton(child: Text("Rosh dead"),
-          onPressed: (){},
-          color: Colors.red,
-          textColor: Colors.yellow,
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-          splashColor: Colors.grey,
+        child: Container(
 
+          child: Column(
+            children:[
+              Text('$count'),
+              RaisedButton(child: Text(count.toString()
+                ,style: TextStyle(
+                  fontSize: 30,
+                ),),
+                onPressed: (){counter();},
+                color: Colors.red,
+                textColor: Colors.yellow,
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                splashColor: Colors.grey,
+
+              ),
+            ]
+          ),
         ),
       ),
     );
+  }
+  void counter(){
+    setState(() {
+      count++;
+    });
   }
 }
