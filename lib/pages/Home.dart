9b4b50e3469458fe395af23aa7cb1adac2268053
samuelskinstad <s_skinstad@hotmail.com>
2020-05-13 18:19:27 +0200

    roshdead

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -10,7 +10,16 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1e2c35),
-      body: Center(child: Container(child: Butt('Rosh dead'))),
+      body: Center(
+        child: RaisedButton(child: Text("Rosh dead"),
+          onPressed: (){},
+          color: Colors.red,
+          textColor: Colors.yellow,
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          splashColor: Colors.grey,
+
+        ),
+      ),
     );
   }
 }
